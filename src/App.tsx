303fafa7d127b63e8daa9b981cb6b70e78cd748f
lukecode <lucassocorrosilva@gmail.com>
2022-10-11 16:48:35 -0300

    Correção e modificação

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,10 @@ function App() {
         />
       </div>
       <section className="section-pomodoro">
-        <h1>Um Temporizador Pomodoro online <br></br>para aumentar sua produtividade</h1>
+        <h1>
+          Um Temporizador Pomodoro online <br></br>para aumentar sua
+          produtividade
+        </h1>
         <article className="article-pomodoro-1">
           <h2 className="subtitle-pomodoro-1">O que é Pomodoro</h2>
           <p>
@@ -38,17 +41,6 @@ function App() {
         </article>
         <article className="article-pomodoro-3">
           <h2 className="subtitle-pomodoro-3">Características</h2>
-          <p>
-            O que é Pomofocus? Pomofocus é um temporizador pomodoro
-            personalizável que funciona no navegador desktop mobile. O objetivo
-            deste aplicativo é ajudá-lo a se concentrar em qualquer tarefa em
-            que você esteja trabalhando, como estudo, escrita ou codificação.
-            Este app é inspirado na Técnica Pomodoro, que é um método de
-            gerenciamento de tempo desenvolvido por Francesco Cirillo.
-          </p>
-        </article>
-        <article className="article-pomodoro-4">
-          <h2 className="subtitle-pomodoro-4">O que é Pomodoro</h2>
           <p>
             O que é Pomofocus? Pomofocus é um temporizador pomodoro
             personalizável que funciona no navegador desktop mobile. O objetivo
